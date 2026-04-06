7934e94ddac1a9b71cb856e7153496d615df0988
badboys88888 <58647847+badboys88888@users.noreply.github.com>
2026-04-07 00:04:16 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,12 +1,10 @@
 import requests
-import time
-import random
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
 }
 
-def fetch_week(url):
+def fetch(url):   # 👈 必须叫这个名字
     r = requests.get(url, headers=HEADERS, timeout=10)
     r.encoding = "utf-8"
     return r.text
