79b072f48867de099ab09379f471faf093bbd370
badboys88888 <58647847+badboys88888@users.noreply.github.com>
2026-04-06 23:59:53 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,36 +1,33 @@
 from datetime import datetime, timedelta
 
-YEAR = 2026
-
-def build_epg(items):
-    result = []
+def build(items, year):
+    epg = []
 
     for i in range(len(items)):
         cur = items[i]
 
         start = datetime.strptime(
-            f"{YEAR}-{cur['date']} {cur['time']}",
+            f"{year}-{cur['date']} {cur['time']}",
             "%Y-%m-%d %H:%M"
         )
 
-        # 默认 end
         if i + 1 < len(items):
-            nxt = items[i + 1]
+            nxt = items[i+1]
+
             end = datetime.strptime(
-                f"{YEAR}-{nxt['date']} {nxt['time']}",
+                f"{year}-{nxt['date']} {nxt['time']}",
                 "%Y-%m-%d %H:%M"
             )
 
-            # 跨天处理
             if end <= start:
                 end += timedelta(days=1)
         else:
             end = start + timedelta(minutes=30)
 
-        result.append({
+        epg.append({
             "start": start.strftime("%Y%m%d%H%M%S"),
             "end": end.strftime("%Y%m%d%H%M%S"),
             "title": cur["title"]
         })
 
-    return result
+    return epg
