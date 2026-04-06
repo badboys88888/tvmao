a244b13a24dcfaf89b9619a7afff0cfe6336efc9
badboys88888 <58647847+badboys88888@users.noreply.github.com>
2026-04-07 00:15:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 CHANNELS = {
     "LUZHOUTV": "https://www.tvmao.com/program/LUZHOUTV-KUZHOUTV1-w1.html",
-    "SCYBTV1": "https://www.tvmao.com/program/SCYBTV-SCYBTV1-w1.html",
-    "SCZGTV1": "https://www.tvmao.com/program/SCZGTV-SCZGTV1-w1.html"
+    "SCYBTV1": "https://www.tvmao.com/program/SCYBTV1-w1.html",
+    "SCZGTV1": "https://www.tvmao.com/program/SCZGTV1-w1.html"
 }
 
 YEAR = 2026
-CACHE_FILE = "cache.json"
 OUTPUT_FILE = "epg.json"
